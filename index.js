fe2554a585cc54f6296e0afd6e4949e1b865d681
bcusack91 <brendan@cusackprep.com>
2021-04-26 12:42:51 -0400

    git init

--- index.js
+++ index.js
@@ -26,9 +26,13 @@ function App() {
     }
 
     return (<>
-        {todos.map((todo, i) => 
-            <Todo index={i} key={i} todo={todo} remove={removeToDo}/>)}
-        <TodoForm addTodo={addTodo}/>
+        <div className="app">
+            <div className="todo-list">
+                {todos.map((todo, i) => 
+                    <Todo index={i} key={i} todo={todo} remove={removeToDo}/>)}
+                <TodoForm addTodo={addTodo}/>
+            </div>
+        </div>
     </>);
 }
 
